e9f1336faaeb28eb2225922ebc21079ad2c2a81a
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-02-26 11:04:57 -0300

    chore: create button component

---
+++ src/components/button/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    button:{
+        width: "100%",
+        height: 52,
+        backgroundColor: "#E15610",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    
+    label:{
+        fontSize: 15,
+        fontWeight: "bold",
+        color: "#FFF",
+    },
+
+});
+
+
+export default styles;
