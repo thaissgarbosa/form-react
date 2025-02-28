307067489f80258e829dd199ec4d0d8e98fa69ee
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-02-28 10:49:41 -0300

    refactor: simplify useState

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,22 @@
 import { Text, View, StyleSheet, } from "react-native";
 import {useState} from "react";
+
 import Button from "../components/button/index";
 import Input from "../components/input";
 
 function Index(){
 
-   const[name, setName] = useState("");// Inferindo a tipagem
-   const[name1, setName1] = useState<string>(""); // Tipo explicitamente declarado
+   const[name, setName] = useState("Usuário");
     
    function handleSubmit() {
         const name = "Thais!";
         alert(`Olá, ${name}`);
     }
 
-    function onChangeText(text: string){
-        console.log(text);
-        setName(text);
-    }
-
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Olá, {name}!</Text>
-            <Input label="Nome" onChangeText={(text) => onChangeText(text)}/>
+            <Input label="Nome" onChangeText={setName}/>
             <Input label="Idade"/>
             <Button label={"Enviar"} onPress={handleSubmit}  activeOpacity={1}/>
             <Button label={"Continuar"} onPress={handleSubmit}  />
